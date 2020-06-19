7983c7f7ae6eaa8dd9a2f4a7b671de52afac1e94
jcgter777 :P <34496783+jcgter777@users.noreply.github.com>
2020-06-19 19:44:19 -0400

    Update basefire.js

--- js/basefire.js
+++ js/basefire.js
@@ -1,3 +1,16 @@
+var firebaseConfig = {
+      apiKey: "AIzaSyC6GNGrAh1tTCU8c_uyqxfEyxVuiv9YIhE",
+      authDomain: "congress-app-challenge-jcjz.firebaseapp.com",
+      databaseURL: "https://congress-app-challenge-jcjz.firebaseio.com",
+      projectId: "congress-app-challenge-jcjz",
+      storageBucket: "congress-app-challenge-jcjz.appspot.com",
+      messagingSenderId: "680387094668",
+      appId: "1:680387094668:web:09cf5cd1524895d3a3f3a9",
+      measurementId: "G-JF18VZ8LDK"
+    };
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+
 document.getElementById('signin-div').addEventListener('click', () => {
 	var provider = new firebase.auth.GoogleAuthProvider();
 	firebase.auth().signInWithPopup(provider).then(function() {});
